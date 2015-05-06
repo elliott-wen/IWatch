8e52d178e522d481f4c5f8aaab86096329aa26e1
Elliott.Wen <jq.elliott.wen@gmail.com>
2015-05-06 22:08:01 +0800

    Fix a bug

--- ffmpeg.py
+++ ffmpeg.py
@@ -24,13 +24,13 @@ class FFMPEG_Watchdog(threading.Thread):
         command = [Config.FFMPEG_BIN,
                         "-f", 'v4l2', '-i','/dev/video0',
                         '-ac','1','-f','alsa','-i','hw:1',
-                        '-ar','22050','-map','0:0','-map','1:0'
+                        '-ar','22050','-map','0:0','-map','1:0',
                         '-r','15','-f',Config.FFMPEG_RTMP_OUTPUT,
                         '-map','0:0','-f','rawvideo','-r','1','unix://'+Config.CAMERA_SOCKET
                         ]
 
         logging.info("Starting FFMPEG")
-        print(command)
+
         subprocess.call(command)
         logging.info("FFMPEG ends!")
 
