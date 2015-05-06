a807bc607f2e21a835d1eeb0781b3ba6bdd0603e
Elliott.Wen <jq.elliott.wen@gmail.com>
2015-05-06 22:10:13 +0800

    Fix a bug

--- ffmpeg.py
+++ ffmpeg.py
@@ -25,7 +25,7 @@ class FFMPEG_Watchdog(threading.Thread):
                         "-f", 'v4l2', '-i','/dev/video0',
                         '-ac','1','-f','alsa','-i','hw:1',
                         '-ar','22050','-map','0:0','-map','1:0',
-                        '-r','15','-f',Config.FFMPEG_RTMP_OUTPUT,
+                        '-r','15','-f','flv',Config.FFMPEG_RTMP_OUTPUT,
                         '-map','0:0','-f','rawvideo','-r','1','unix://'+Config.CAMERA_SOCKET
                         ]
 
