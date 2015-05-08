7d9ca8d05248080e02024dde98eba2b270c76be2
Elliott.Wen <jq.elliott.wen@gmail.com>
2015-05-08 13:14:26 +0800

    debugging

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config():
      FFMPEG_COMMAND  = [FFMPEG_BIN,
                         "-f", 'v4l2', '-i','/dev/video0',#'-video_size','%dx%d'%(FFMPEG_FRAME_WIDTH,FFMPEG_FRAME_HEIGHT),
                         '-ac','1','-f','alsa','-i','hw:1',
-                        '-ar','22050','-map','0:0','-map','1:0',
-                        '-r','15','-f','flv',FFMPEG_RTMP_OUTPUT,
+                        '-ar','22050',
+                        #'-map','0:0','-map','1:0','-r','15','-f','flv',FFMPEG_RTMP_OUTPUT,
                         '-map','0:0','-f','rawvideo','-r','1','unix://'+ CAMERA_SOCKET
                         ]
