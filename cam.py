48476cb9684936836c3373b81c81746fd835bb8e
Elliott.Wen <jq.elliott.wen@gmail.com>
2015-05-09 17:05:22 +0800

    debugging

--- cam.py
+++ cam.py
@@ -59,7 +59,7 @@ class Camera(threading.Thread):
         self.detect_motion(img)
 
     def detect_motion(self,img):
-        if self.lastImg == None:
+        if self.lastImg:
             self.lastImg = img
             return False
         diff = cv2.absdiff(self.lastImg,img)
@@ -68,6 +68,6 @@ class Camera(threading.Thread):
         diff = cv2.morphologyEx(diff, cv2.MORPH_OPEN, kernel)
         diff = cv2.morphologyEx(diff, cv2.MORPH_CLOSE, kernel)
         diff = cv2.threshold(diff,10,255,cv2.THRESH_BINARY)
-
+        print(diff)
         self.lastImg = img
         return False
