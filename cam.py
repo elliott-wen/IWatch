799891b42dd7bd4d8a22d205416df77c6add16a9
Elliott.Wen <jq.elliott.wen@gmail.com>
2015-05-09 17:21:10 +0800

    debugging

--- cam.py
+++ cam.py
@@ -68,6 +68,6 @@ class Camera(threading.Thread):
         diff = cv2.morphologyEx(diff, cv2.MORPH_OPEN, kernel)
         diff = cv2.morphologyEx(diff, cv2.MORPH_CLOSE, kernel)
         diff = cv2.threshold(diff,10,255,cv2.THRESH_BINARY)
-        print(diff)
+        print(np.linalg.norm(diff))
         self.lastImg = img
         return False
